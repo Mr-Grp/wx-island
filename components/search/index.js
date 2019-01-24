54bac1986c7943809e3292fb1bd28d00313e1919
i-guoruipeng@shuwen.com <i-guoruipeng@shuwen.com>
2019-01-24 14:19:23 +0800

    完成书籍搜索

--- components/search/index.js
+++ components/search/index.js
@@ -1,23 +1,117 @@
 // components/search/search.js
+import SearchModel from '../../models/search.js'
+import BookModel from '../../models/book.js'
+import paginationBev from '../behaviors/pagination.js'
+const searchModel = new SearchModel()
+const bookModel = new BookModel()
+
 Component({
-  /**
-   * 组件的属性列表
-   */
+
+  behaviors: [paginationBev],
   properties: {
-
+    more: {
+      type: String,
+      observer: 'loadMore'
+    }
   },
 
-  /**
-   * 组件的初始数据
-   */
+  attached() {
+    this.setData({
+      historyWords: searchModel.getHistory()
+    })
+
+    searchModel.getHot()
+      .then(res => {
+        this.setData({
+          hotWords: res.hot
+        })
+      })
+  },
+
   data: {
-
+    historyWords: [],
+    hotWords: [],
+    searching: false,
+    q: '',
+    loading: false,
+    loadingCenter: false
   },
 
-  /**
-   * 组件的方法列表
-   */
   methods: {
 
+    onConfirm(event) {
+      this._showResult()
+      this._showLoadingCenter()
+      this.initialize()
+      const q = event.detail.value || event.detail.text
+      this.setData({
+        q
+      })
+      bookModel.search(0, q)
+        .then(res => {
+          this.setMoreData(res.books)
+          this.setTotal(res.total)
+          searchModel.addToHistory(q)
+          this._hideLoadingCenter()
+        })
+    },
+
+    onCancel(event) {
+      this.triggerEvent('cancel', {}, {})
+    },
+
+    onDelete(event) {
+      this.initialize()
+      this._closeResult()
+    },
+
+    onBookTap(event) {
+      wx.navigateTo({
+        url: `/pages/book-detail/book-detail?bid=${event.detail}`
+      })
+    },
+
+    loadMore() {
+      if (!this.data.q) {
+        return
+      }
+      if (this.isLocked()) {
+        return
+      }
+      if (this.hasMore()) {
+        this.locked()
+        bookModel.search(this.getCurrentStart(), this.data.q)
+          .then(res => {
+            this.setMoreData(res.books)
+            this.unLocked()
+          }, () => {
+            this.unLocked()
+          })
+      }
+    },
+
+    _showLoadingCenter() {
+      this.setData({
+        loadingCenter: true
+      })
+    },
+
+    _hideLoadingCenter() {
+      this.setData({
+        loadingCenter: false
+      })
+    },
+    _showResult() {
+      this.setData({
+        searching: true
+      })
+    },
+
+    _closeResult() {
+      this.setData({
+        searching: false,
+        q: ''
+      })
+    }
   }
 })
